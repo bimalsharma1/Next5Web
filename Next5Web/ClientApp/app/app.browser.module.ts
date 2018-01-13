d32acfe575eeac9ddddcf8d2d1b462babb4afa05
bimalsharma1 <bimalsharma1@outlook.com>
2018-01-13 15:30:43 +1000

    make secure call

--- Next5Web/ClientApp/app/app.browser.module.ts
+++ Next5Web/ClientApp/app/app.browser.module.ts
@@ -17,5 +17,5 @@ export class AppModule {
 }
 
 export function getBaseUrl() {
-    return "http://next5api20180107093126.azurewebsites.net/";
+    return "https://next5api20180107093126.azurewebsites.net/";
 }
